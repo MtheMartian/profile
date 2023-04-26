f47ac1acd71f5cd4ef8e14ab66e59fff2a531e5e
MtheMartian <marvin.altidor@outlook.com>
2023-04-25 23:05:00 -0400

    about me

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,5 +1,12 @@
-import React, {MouseEventHandler, useEffect} from 'react';
+import React, {MouseEventHandler, useEffect, useState} from 'react';
 import '../css/profile.css';
+import Twitter from '../assets/twitter-blue.png';
+import GitHub from '../assets/github-dark-64px.png';
+import LinkedIn from '../assets/lin.png';
+import CSharp from '../assets/tool-logo/c-sharp-c-seeklogo.com.svg';
+import JavaScript from '../assets/tool-logo/javascript-js-seeklogo.com.svg';
+import Node from '../assets/tool-logo/nodejs-seeklogo.com.svg';
+import ReactJS from '../assets/tool-logo/react-seeklogo.com.svg';
 import $ from 'jquery';
 
 interface GeneralProps {
@@ -38,30 +45,48 @@ function SideMenu({fillBorderOnClick} : GeneralProps){
 }
 
 function AboutMe({fillBorderOnClick} : GeneralProps){
-  const tools: {name: string, image: string}[] = [{name: "C#",image: ""}, {name: "JavaScript",image: ""},
-  {name: "React",image: ""}, {name: "EJS",image: ""}, {name: "Nodejs",image: ""}];
+  const tools: {name: string, image: string}[] = [{name: "C#",image: CSharp}, {name: "JavaScript",image: JavaScript},
+  {name: "React",image: ReactJS}, {name: "EJS",image: ""}, {name: "NodeJS",image: Node}];
+
+  const links : string[] = [GitHub, LinkedIn, Twitter];
+
+  const [displayLinks, setDisplayLinks] = useState<boolean>(false);
+
+  useEffect(() =>{
+    const linksTimer: NodeJS.Timer = setTimeout(() =>{
+      setDisplayLinks(prev => prev = true);
+    }, 2000);
+
+    return () =>{
+      clearInterval(linksTimer);
+    }
+  });
 
   return(
     <section id="about-me" className="portfolio-section-general">
       <div id="olo">
-        <div>
-          <h2 id="name">Marvin Altidor</h2>
+        <div id="title-name-links">
+          <div id="name-wrapper">
+            <h2 id="name">Marvin Altidor</h2>
+            {displayLinks ? <div id="links-wrapper">
+            {links.map((link: string, index: number) => 
+              <img alt="link" src={link} className="link-img"/>
+            )}
+            </div> : null}
+          </div>
           <h1 id="what-am-i">Web Developer</h1>
-          <div>links</div>
         </div>
         <p>I'm a Full Stack developper and I bring ideas to life.</p>
         <p>Tools used to make this happen:</p>
-        <div>
-          <ul>
-            {tools.map((tool: {name: string, image: string}, index: number) =>
-            <li key={index}>
-              <img alt="tool" src={tool.image} />
-              <span>{tool.name}</span>
-            </li>
-            )}
-          </ul>
+        <ul id="tools-wrapper">
+          {tools.map((tool: {name: string, image: string}, index: number) =>
+          <li key={index} className="tool">
+            <img alt="tool" src={tool.image} className="tool-img"/>
+            <span className="tool-tooltip">{tool.name}</span>
+          </li>
+          )}
+        </ul>
           <a href="#projects" className="side-menu-anchors" onClick={fillBorderOnClick}>PROJECTS</a>
-        </div>
       </div>
     </section>
   );
